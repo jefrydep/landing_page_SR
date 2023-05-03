2c2880a6bdbb8f6d78109d76ee72d984819e673f
jefry <jefrydep@gmail.com>
2023-05-02 19:14:41 -0500

    ready to hange

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,24 @@ function closeMenu(){
   backgroundMenu.style.display= "none";
 
 }
+
+
+  //  // Obtenemos todas las secciones y las ocultamos por defecto, excepto la de inicio.
+  //  var secciones = document.getElementsByTagName("section");
+  //  for (var i = 1; i < secciones.length; i++) {
+  //    secciones[i].style.display = "none";
+  //  }
+
+  //  // Escuchamos el evento de clic en cada enlace de navegación.
+  //  var enlaces = document.getElementsByTagName("a");
+  //  for (var i = 0; i < enlaces.length; i++) {
+  //    enlaces[i].addEventListener("click", function(evento) {
+  //      evento.preventDefault(); // Evita que se recargue la página al hacer clic en un enlace.
+  //      var objetivo = this.getAttribute("href"); // Obtenemos el valor del atributo href del enlace.
+  //      var seccionObjetivo = document.querySelector(objetivo); // Buscamos la sección correspondiente.
+  //      for (var j = 0; j < secciones.length; j++) {
+  //        secciones[j].style.display = "none"; // Ocultamos todas las secciones.
+  //      }
+  //      seccionObjetivo.style.display = "block"; // Mostramos la sección correspondiente.
+  //    });
+  //  }
